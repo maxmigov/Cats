17aa02e2e5aa97e782c962e2b806068a7c4a801c
maxmigov <60349576+maxmigov@users.noreply.github.com>
2020-02-14 16:01:03 +0200

    new updates

--- com/data/Cat.java
+++ com/data/Cat.java
@@ -1,50 +1,76 @@
 package com.data;
 
 public class Cat implements FelineInterface {
-    private String name;
-    private String race;
-    private int year;
+    String name;
+    String race;
+    int year;
+    Integer id;
 
-    public Cat(String name, String race, int year){
+    public Cat(String name, String race, int year, Integer id) {
         this.name = name;
         this.race = race;
         this.year = year;
+        this.id = id;
     }
 
-
-    @Override
-    public void setName(String name) {
-
+    public Cat(String name, String race, int year) {
     }
+
 
     @Override
     public String getName() {
-        return null;
-    }
-
-    @Override
-    public void setRace(String race) {
-
+        return name;
     }
 
     @Override
     public String getRace() {
-        return null;
-    }
-
-    @Override
-    public void setYear(int year) {
-
+        return race;
     }
 
     @Override
     public int getYear() {
-        return 0;
+        return year;
     }
 
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    @Override
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        FelineInterface feline = (FelineInterface) super.clone();
+        feline.setName(this.name);
+        feline.setRace(this.race);
+        feline.setYear(this.year);
+        return feline;
+    }
+
+    @Override
     public String toString() {
-        return "Name" + name
-                + "Race" + race
-                + "Year" + year;
+        return "Cat { " +
+                "name = '" + name + '\'' +
+                ", race = '" + race + '\'' +
+                ", year = " + year +
+                '}';
     }
 }
--- com/data/FelineInterface.java
+++ com/data/FelineInterface.java
@@ -1,11 +1,16 @@
 package com.data;
 
 public interface FelineInterface {
+
     public void setName(String name);
     public String getName();
+
     public void setRace(String race);
     public String getRace();
+
     public void setYear(int year);
     public int getYear();
-}
 
+    public void setId(Integer id);
+    public Integer getId();
+}
